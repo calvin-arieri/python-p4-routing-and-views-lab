0b1c78eb220cb910d08f9837daa90953011912c4
calvin-arieri <morebucalvin@gmail.com>
2023-06-20 15:04:16 -0700

    completed assignment

--- server/app.py
+++ server/app.py
@@ -20,18 +20,18 @@ def count(param):
 @app.route('/math/<int:num1><string:operation><int:num2>')
 def math(num1, num2, operation):
     if operation == '+':
-        return f'<p>{num1 + num2}</p>\n'
+        return f'{num1 + num2}\n'
     elif operation == '-':
-        return f'<p>{num1 - num2}</p>\n'
+        return f'{num1 - num2}\n'
     elif operation == '*':
-        return f'<p>{num1 * num2}</p>\n'
+        return f'{num1 * num2}\n'
     elif operation == 'div':
         if num2 != 0:
-            return f'<p>{num1 / num2}</p>\n'
+            return f'{num1 / num2}\n'
         else:
             return 'Error: Division by zero is not allowed.\n'
     elif operation == '%':
-        return f'<p>{num1 % num2}</p>\n'
+        return f'{num1 % num2}\n'
     else:
         return 'Operation not recognized. Please use one of the following: + - * div %'
     
