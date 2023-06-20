05b059697b22339539aa1f7dd994ab0621bc1557
calvin-arieri <morebucalvin@gmail.com>
2023-06-20 14:59:15 -0700

    completed assignment

--- server/app.py
+++ server/app.py
@@ -4,5 +4,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:param>')
+def count(param):    
+    return '\n'.join(str(num) for num in range(0, param + 1)) 
+
+@app.route('/math/<int:num1><string:operation><int:num2>')
+def math(num1, num2, operation):
+    if operation == '+':
+        return f'<p>{num1 + num2}</p>\n'
+    elif operation == '-':
+        return f'<p>{num1 - num2}</p>\n'
+    elif operation == '*':
+        return f'<p>{num1 * num2}</p>\n'
+    elif operation == 'div':
+        if num2 != 0:
+            return f'<p>{num1 / num2}</p>\n'
+        else:
+            return 'Error: Division by zero is not allowed.\n'
+    elif operation == '%':
+        return f'<p>{num1 % num2}</p>\n'
+    else:
+        return 'Operation not recognized. Please use one of the following: + - * div %'
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
